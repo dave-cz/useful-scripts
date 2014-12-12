e5b55874d41613737c15ba55f8884d505d40b8bc
dave-cz <david.charvat.cz@gmail.com>
2014-12-12 13:56:30 +0100

    AutoReconnect.py - change exception

--- python/AutoReconnect.py
+++ python/AutoReconnect.py
@@ -13,33 +13,42 @@ from datetime import datetime
 from time import sleep
 from argparse import ArgumentParser
 
-def is_internet_on(timeout = 5, url = 'http://www.google.cz'):
-    try:
-        urlopen(url, timeout = timeout)
-        return True
-    except URLError: pass # as err
-    return False
+class AutoReconnect:
+	def __init__ (self, args):
+		""" Class initialiser """
+		self.__dict__.update(args.__dict__)
+		self.url = 'http://www.google.com'
+		self.command = 'netsh.exe wlan connect name="{}" ssid="{}"'
+	
+	def loop(self):
+		while True:
+			if self.is_internet_on():
+				print "OK:", datetime.now().time()
+			else:
+				print "Restarting WiFi Connection", datetime.now().time()
+				self.reconnect()
+				print "\nRestarting WiFi Done", datetime.now().time()
+			sleep(self.sleeptime)
 
-def reconnect_wifi(name, ssid):
-	try:
-		system('netsh.exe wlan connect name="{}" ssid="{}"'.format(name, ssid))
-	except Exception, ex:
-		print ex
+	def is_internet_on(self):
+		try:
+			urlopen(self.url, timeout = self.timeout)
+			return True
+		except Exception, ex: pass
+		return False
+	
+	def reconnect(self):
+		try:
+			system(self.command.format(self.name, self.ssid))
+		except Exception, ex:
+			print ex
 
 if __name__ == '__main__':
 	parser = ArgumentParser()
 	parser.add_argument('-s', '--sleeptime', type=int, help="loop period, default = 30 s", default=30)
-	parser.add_argument('-t', '--timeout',   type=int, help="server request timeout, default = 5 s", default=5)
+	parser.add_argument('-t', '--timeout', type=int, help="server request timeout, default = 5 s", default=5)
+	parser.add_argument('-n', '--name', help="Wifi Network Name, default = 'SviZaCha'", default='SviZaCha')
+	parser.add_argument('-ssid', '--ssid', help="Wifi Network SSID, default = 'SviZaCha'", default='SviZaCha')
 	args = parser.parse_args()
 	
-	print "\nTimeout: {} s\nSleep Time: {} s\n".format(args.timeout, args.sleeptime)
-	while True:
-		if is_internet_on(args.timeout):
-			print "OK:", datetime.now().time()
-		else:
-			print "Restarting WiFi Connection", datetime.now().time()
-			reconnect_wifi('SviZaCha', 'SviZaCha')
-			print "\nRestarting WiFi Done", datetime.now().time()
-			
-		sleep(args.sleeptime)
-	
+	AutoReconnect(args).loop()
