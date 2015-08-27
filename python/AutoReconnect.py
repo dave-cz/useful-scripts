81edef7c1763980b41f552b79332d29132206dd7
dave-cz <david.charvat.cz@gmail.com>
2015-08-27 13:43:34 +0200

    add setting Miranda online after reconnect; refactoring

--- python/AutoReconnect.py
+++ python/AutoReconnect.py
@@ -1,59 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#  AutoReconnect.py
-#
-#  Reconnect WiFi when internet is down
-#
+"""
+AutoReconnect
 
-import os, sys
-from urllib2 import urlopen, URLError
+Reconnect WiFi when internet is down
+
+TODO: start Wi-Fi, if off: Win+X, T
+"""
+import os
+from WindowFinder import WindowFinder
+from SendKeys import SendKeys
+from urllib2 import urlopen#, URLError
 from datetime import datetime
 from time import sleep
 from argparse import ArgumentParser
 
-class AutoReconnect:
-	def __init__ (self, args):
-		""" Class initialiser """
-		self.__dict__.update(args.__dict__)
-		self.url = 'http://www.google.com'
-		self.command = 'netsh.exe wlan connect name="{}" ssid="{}"'
-		self.logdir = os.path.dirname(os.path.abspath(__file__))
-		self.logfile = 'timeout.log'
-	
-	def loop(self):
-		while True:
-			if self.is_internet_on():
-				print "OK:", datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-			else:
-				self.log('Connection timeout at '+datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-				self.reconnect()
-			sleep(self.sleeptime)
+class AutoReconnect(object):
+    """ Reconnect WiFi when internet is down """
+    def __init__(self, args=None):
+        """ Class initialiser """
+        self.sleeptime = args['sleeptime']
+        self.timeout = args['timeout']
+        self.name = args['name']
+        self.ssid = args['ssid']
+        self.miranda = args['miranda']
+        self.url = 'http://www.google.com'
+        self.command = 'netsh.exe wlan connect name="{}" ssid="{}"'
+        self.logdir = os.path.dirname(os.path.abspath(__file__))
+        self.logfile = 'timeout.log'
 
-	def is_internet_on(self):
-		try:
-			urlopen(self.url, timeout = self.timeout)
-			return True
-		except Exception, ex: pass
-		return False
-	
-	def reconnect(self):
-		try:
-			os.system(self.command.format(self.name, self.ssid))
-		except Exception, ex:
-			print ex
-	
-	def log(self, message):
-		print 'Log: ', message
-		with open(self.logdir+'\\'+self.logfile, 'ab+') as f:
-			f.write(message+'\n')
-	
+    def loop(self):
+        while True:
+            if self.is_internet_on():
+                print "OK:", datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            else:
+                self.log('Connection timeout at '+datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                self.reconnect()
+                if self.miranda:
+                    sleep(10)
+                    try:
+                        self.set_miranda_online()
+                    except Exception, ex:
+                        print ex
+            sleep(self.sleeptime)
+
+    def is_internet_on(self):
+        try:
+            urlopen(self.url, timeout=self.timeout)
+            return True
+        except Exception:
+            pass
+        return False
+
+    def reconnect(self):
+        try:
+            os.system(self.command.format(self.name, self.ssid))
+        except Exception, ex:
+            print ex
+
+    def log(self, message):
+        print 'Log: ', message
+        with open(self.logdir+'\\'+self.logfile, 'ab+') as fh_log:
+            fh_log.write(message+'\n')
+
+    @staticmethod
+    def set_miranda_online():
+        """
+            Find Miranda window, activate and set online
+        """
+        win = WindowFinder()
+        win.find_window_wildcard('Miranda')
+        win.set_foreground()
+        SendKeys('^(+)') # Ctrl + '+'
+        SendKeys('{ENTER}') # Confirm window 'Change online message'
+
 if __name__ == '__main__':
-	parser = ArgumentParser()
-	parser.add_argument('-s', '--sleeptime', type=int, help="loop period, default = 30 s", default=30)
-	parser.add_argument('-t', '--timeout', type=int, help="server request timeout, default = 5 s", default=5)
-	parser.add_argument('-n', '--name', help="Wifi Network Name, default = 'SviZaCha'", default='SviZaCha')
-	parser.add_argument('-ssid', '--ssid', help="Wifi Network SSID, default = 'SviZaCha'", default='SviZaCha')
-	args = parser.parse_args()
-	
-	AutoReconnect(args).loop()
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--sleeptime', type=int, help="loop period, default = 30 s", default=30)
+    parser.add_argument('-t', '--timeout', type=int, help="server request timeout, default = 5 s", default=5)
+    parser.add_argument('-n', '--name', help="Wifi Network Name, default = 'SviZaCha'", default='SviZaCha')
+    parser.add_argument('-ssid', '--ssid', help="Wifi Network SSID, default = 'SviZaCha'", default='SviZaCha')
+    parser.add_argument('-m', '--miranda', help="Set Miranda IM online, default = True", default=True)
+    
+    args_reconnect = vars(parser.parse_args())
+    AutoReconnect(args_reconnect).loop()
---
+++ python/WindowFinder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+WindowFinder
+
+Source: http://stackoverflow.com/a/17350440
+"""
+
+import win32gui, re #, SendKeys
+
+class WindowFinder(object):
+    """ Class to find and make focus on a particular Native OS dialog/Window """
+    def __init__(self):
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        """ Pass a window class name & window name directly if known to get the window """
+        self._handle = win32gui.FindWindow(class_name, window_name)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        """ Call back func which checks each open window and matches the name of window using reg ex """
+        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) != None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        """ This function takes a string as input and calls EnumWindows to enumerate through all open windows """
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        """ Get the focus on the desired open window """
+        win32gui.SetForegroundWindow(self._handle)
+
+if __name__ == '__main__':
+    pass
+    #win = WindowFinder()
+    #win.find_window_wildcard(".*Save As.*")
+    #win.set_foreground()
+    #path = "D:\\File.txt"            #Path of the file you want to Save
+    #ent = "{ENTER}"                  #Enter key stroke.
+    #SendKeys.SendKeys(path)          #Use SendKeys to send path string to Save As dialog
+    #SendKeys.SendKeys(ent)           #Use SendKeys to send ENTER key stroke to Save As dialog
